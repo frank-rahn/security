6cfbf088a517d1bb4c963287a16b3e587cf6724f
Frank W. Rahn <frank@frank-rahn.de>
2014-03-08 22:01:12 +0100

    Package Struktur und erste Implementierungen

---
+++ java-security-api/java-security-api-common/src/main/java/de/rahn/security/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * In diesem Package liegen die Utilities und Funktionen rund um die Java-
+ * Security-API bzw. die Java Cryptography Extension (JCE).
+ * @author Frank W. Rahn
+ */
+package de.rahn.security.api;
---
+++ java-security-api/java-security-api-provider/src/main/java/de/rahn/security/api/provider/Main.java
@@ -0,0 +1,15 @@
+package de.rahn.security.api.provider;
+
+/**
+ * @author Frank W. Rahn
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
