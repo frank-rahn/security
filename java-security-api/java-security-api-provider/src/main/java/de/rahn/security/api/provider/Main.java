e4b159f712c8b3709a64adac0ec3df1bc48d25ff
Frank W. Rahn <frank@frank-rahn.de>
2014-03-09 19:29:42 +0100

    Erste Ausgabe implementiert

--- java-security-api/java-security-api-provider/src/main/java/de/rahn/security/api/provider/Main.java
+++ java-security-api/java-security-api-provider/src/main/java/de/rahn/security/api/provider/Main.java
@@ -1,15 +1,38 @@
 package de.rahn.security.api.provider;
 
+import java.security.Provider;
+import java.security.Security;
+
+import de.rahn.security.api.SecurityPrinter;
+
 /**
  * @author Frank W. Rahn
  */
-public class Main {
+public class Main extends SecurityPrinter {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		try (Main main = new Main()) {
+			main.run();
+		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		appendTitle("Die Liste der verfügbaren Security-Provider");
+
+		for (Provider provider : Security.getProviders()) {
+			appendDesc("Security Provider Info:")
+				.appendValue("Name", provider.getName())
+				.appendValue("Version", provider.getVersion())
+				.appendValue("Info", provider.getInfo())
+				.appendToString(provider);
+		}
+	}
 }
